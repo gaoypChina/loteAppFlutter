f3204e6cc3cf7c3a73232f37e6546565930ebdce
Jean Contreras <jeancon29@gmail.com>
2020-07-02 13:39:00 -0400

    Se soluciono problema de verTicket en el monitoreo

--- lib/core/services/ticketservice.dart
+++ lib/core/services/ticketservice.dart
@@ -125,7 +125,9 @@ class TicketService{
     map["codigoQr"] = codigoQr;
     map["idUsuario"] = await Db.idUsuario();
     map["idBanca"] = await Db.idBanca();
-    mapDatos["datos"] = map;
+    map["servidor"] = await Db.servidor();
+    var jwt = await Utils.createJwt(map);
+    mapDatos["datos"] = jwt;
 
     var response = await http.post(Utils.URL + "/api/principal/buscarTicket", body: json.encode(mapDatos), headers: Utils.header);
     int statusCode = response.statusCode;
