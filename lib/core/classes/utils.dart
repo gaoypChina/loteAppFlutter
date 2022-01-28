e48f3d213de5a29b9d74841d89768137c3b3510b
Jeancito <jeancon29@gmail.com>
2022-01-28 19:10:44 -0400

    Copia

--- lib/core/classes/utils.dart
+++ lib/core/classes/utils.dart
@@ -32,7 +32,7 @@ class  Utils {
   // static final String URL = 'https://pruebass.ml';
   // static final String URL = 'http://127.0.0.1:8000';
 
-  static final String URL = 'https://loteriasdo.gq';
+  static final String URL = 'https://server.loteriasdo.gq';
   static final String URL_SOCKET = URL.replaceFirst("https", "http") + ":3000";
   // static final String URL_SOCKET = URL + ":3000";
   
